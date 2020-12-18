cdd5cebf6029a591c3ae1044fbce4705422dbda9
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-12-18 10:20:15 +0530

    Use RayTracer to output an image of sky

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
+use raytracer::{
+    camera::Camera,
+    primitive::Vec3,
+    ray_tracer::{Config, RayTracer},
+};
+
 const WD: u32 = 200;
 const HT: u32 = 100;
 
 fn main() {
+    let ray_tracer = RayTracer::new();
+    let config = Config {
+        canvas_wd: WD,
+        canvas_ht: HT,
+        sky_color: Vec3::new(0.5, 0.7, 1.0),
+        camera: Camera::new(Vec3::all(0.0), 90.0, WD as f32 / HT as f32),
+    };
+
     println!("P3\n{} {}\n255", WD, HT);
 
     for j in (0..HT).rev() {
         for i in 0..WD {
-            let r = i as f32 / WD as f32;
-            let g = j as f32 / HT as f32;
-            let b = 0.2;
+            let color = ray_tracer.color_pixel(&config, i, j);
 
-            let r = (255.99 * r) as u8;
-            let g = (255.99 * g) as u8;
-            let b = (255.99 * b) as u8;
+            let r = (255.99 * color.x) as u8;
+            let g = (255.99 * color.y) as u8;
+            let b = (255.99 * color.z) as u8;
 
             println!("{} {} {}", r, g, b);
         }
