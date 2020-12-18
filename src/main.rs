8cf697198d92881fe8eb1070da364424f444c198
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-12-18 08:42:07 +0530

    Test PPM output by printing a simple gradient

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+const WD: u32 = 200;
+const HT: u32 = 100;
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n{} {}\n255", WD, HT);
+
+    for j in (0..HT).rev() {
+        for i in 0..WD {
+            let r = i as f32 / WD as f32;
+            let g = j as f32 / HT as f32;
+            let b = 0.2;
+
+            let r = (255.99 * r) as u8;
+            let g = (255.99 * g) as u8;
+            let b = (255.99 * b) as u8;
+
+            println!("{} {} {}", r, g, b);
+        }
+    }
 }
