ddb2d998b8a3d49adf9283847ed6eac060b7fb1a
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-12-18 11:24:50 +0530

    Update RayTracer to render scenes using geometry normals
    The colors are obviously inaccurate. But they're helping for debugging
    purposes.

--- src/ray_tracer.rs
+++ src/ray_tracer.rs
@@ -1,4 +1,5 @@
 use crate::camera::Camera;
+use crate::geometry::Scene;
 use crate::primitive::{Ray3, Vec3};
 
 pub struct RayTracer {}
@@ -15,16 +16,20 @@ impl RayTracer {
         RayTracer {}
     }
 
-    pub fn color_pixel(&self, config: &Config, i: u32, j: u32) -> Vec3 {
+    pub fn color_pixel(&self, scene: &Scene, config: &Config, i: u32, j: u32) -> Vec3 {
         let u = (i as f32) / config.canvas_wd as f32;
         let v = (j as f32) / config.canvas_ht as f32;
 
         let ray = config.camera.get_ray(u, v);
-        self.color(&ray, config)
+        self.color(&ray, scene, config)
     }
 
-    fn color(&self, ray: &Ray3, config: &Config) -> Vec3 {
-        let t = 0.5 * (ray.dir.normalized().y + 1.0);
-        (1.0 - t) * Vec3::all(1.0) + t * config.sky_color
+    fn color(&self, ray: &Ray3, scene: &Scene, config: &Config) -> Vec3 {
+        if let Some(hit) = scene.hit(ray, 0.0, f32::MAX) {
+            0.5 * hit.normal + Vec3::all(0.5)
+        } else {
+            let t = 0.5 * (ray.dir.normalized().y + 1.0);
+            (1.0 - t) * Vec3::all(1.0) + t * config.sky_color
+        }
     }
 }
